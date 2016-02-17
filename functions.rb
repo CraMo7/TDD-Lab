fc52ecb55caa2de1e6a327c1838fe7f83876f065
Craig Morton <CraigMorton92@gmail.com>
2016-02-17 17:06:14 +0000

    solved problem 3 - hogwarts array search

--- functions.rb
+++ functions.rb
@@ -15,9 +15,26 @@ def sum_array_of_ints(array)
 end
 
 def find_ravenclaw_in_array(array, search_term)
-
+  count = 0
+  while count < array.length
+    if array[count] == search_term
+      return true
+    end  
+    count += 1
+  end  
 end
 
 def get_first_key_name_in_hash(hash, index)
   return hash.keys[index]
+end
+
+def get_capitals_from_nested_hash(top_level_hash)
+  count = 0
+  num_of_countries = top_level_hash.keys.length
+  array_of_capitals = []
+    while count < num_of_countries
+      current_country_number = top_level_hash.keys[count]
+     array_of_capitals[count] = current_country_number[:capital]
+
+    end
 end
